2b3c160eda93ad14033acbad6d0dcf755eeb183f
guna-ui <gunasekhar449@live.com>
2024-12-25 12:02:23 +0530

    looping into dictionaries

--- dictionaries.py
+++ dictionaries.py
@@ -7,3 +7,14 @@ person={
     'Last Name' : "Idle",
     "year Born" : 2023
 }
+
+#looping into dictionaries
+
+person={
+    'firstname':'guna',
+    'lastname':'sekhar',
+    'age':32,
+}
+
+for keys,values in person.items():
+    print(f'{keys}--->{values}')
