75175f160f576fe14b427ba004ef9cddae5cbe43
guna-ui <your_email@example.com>
2024-12-27 18:24:42 +0530

    added sets in exercise

--- exercises.py
+++ exercises.py
@@ -64,4 +64,19 @@ for key,val in grades.items():
        
 print(grades)
 
+data = {
+    'd1': {'a': 1, 'b': 2, 'c': 3},
+    'd2': {'b': 20, 'c': 30, 'd': 40},
+    'd3': {'d': 100, 'x': 200}
+}
+
+result=set()
+
+for key,val in data.items():
+    # a,b=data[key].items()
+    for k,v in data[key].items():
+        result.add(k)
+
+print(result)
+
     
